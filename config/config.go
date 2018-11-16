daed1d05ac0b12b66fc811d829a1e62205fd3d76
Joe Groocock <me@frebib.net>
2018-11-16 11:30:51 +0000

    Avoid -h/--help conflict with --host

--- config/config.go
+++ config/config.go
@@ -7,7 +7,7 @@ import (
 )
 
 type Options struct {
-	Host         string `short:"h" long:"host" description:"Minecraft server host address" env:"RCON_HOST" required:"true"`
+	Host         string `short:"H" long:"host" description:"Minecraft server host address" env:"RCON_HOST" required:"true"`
 	Port         uint   `short:"p" long:"port" description:"Minecraft server RCON port" env:"RCON_PORT" default:"25575"`
 	Password     string `short:"P" long:"password" description:"Minecraft server RCON password" env:"RCON_PASS" required:"true"`
 	Provider     string `long:"provider" description:"Backup provider, for taking/storing backups" env:"BACKUP_PROVIDER" default:"tar" choice:"zfs" choice:"zip" choice:"tar"`
